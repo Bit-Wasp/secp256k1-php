745cf6bbf54f6f5524ba10f28330be7287edd509
Thomas Kerin <thomas@bitwasp.co>
2015-04-30 16:32:08 +0100

    Add consistency tests for import/export

---
+++ tests/Secp256k1PrivkeyImportExportTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BitWasp\Secp256k1Tests;
+
+
+use Symfony\Component\Yaml\Yaml;
+
+class Secp256k1PrivkeyImportExportTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function getPkVectors()
+    {
+        $parser = new Yaml();
+        $data = $parser->parse(__DIR__ . '/data/pubkey_create.yml');
+
+        $fixtures = array();
+        foreach ($data['vectors'] as $c => $vector) {
+            $fixtures[] = array(
+                $vector['seckey'],
+                ($c%2 == 0)
+            );
+        }
+        return $fixtures;
+    }
+
+    /**
+     * @dataProvider getPkVectors
+     * @param $seckey
+     * @param $compressed
+     */
+    public function testImportExport($seckey, $compressed)
+    {
+        $seckey = $this->toBinary32($seckey);
+
+        $der = '';
+        $r = secp256k1_ec_privkey_export($seckey, $compressed, $der);
+        $this->assertEquals(1, $r);
+
+        $recovered = '';
+        $r = secp256k1_ec_privkey_import($der, $recovered);
+        $this->assertEquals(1, $r);
+
+
+        $this->assertEquals($seckey, $recovered);
+
+    }
+}
