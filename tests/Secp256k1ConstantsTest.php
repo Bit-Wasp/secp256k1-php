67132fba51b75e8ef28fe6216db61ed1ac827d3a
Thomas Kerin <afk11@users.noreply.github.com>
2015-08-12 18:16:58 +0100

    Add tests file for exported constants

---
+++ tests/Secp256k1ConstantsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BitWasp\Secp256k1Tests;
+
+class Secp256k1ConstantsTest extends TestCase
+{
+    public function testConstants()
+    {
+        $this->assertEquals('secp256k1_ecdsa_signature_t', SECP256K1_TYPE_SIG);
+        $this->assertEquals('secp256k1_pubkey_t', SECP256K1_TYPE_PUBKEY);
+        $this->assertEquals('secp256k1_context_t', SECP256K1_TYPE_CONTEXT);
+
+        $this->assertEquals('1', SECP256K1_CONTEXT_VERIFY);
+        $this->assertEquals('2', SECP256K1_CONTEXT_SIGN);
+    }
+}
