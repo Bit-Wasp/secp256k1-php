e1989e41b3ba728c3ca31c90b726ec1ac105bbd7
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-02-09 18:42:19 +0100

    more tests

---
+++ secp256k1/tests/secp256k1_ecdsa_sign_error3.php
@@ -0,0 +1,28 @@
+--TEST--
+secp256k1_ecdsa_sign errors if provided an invalid resource as a context
+--SKIPIF--
+<?php
+if (!extension_loaded("secp256k1")) print "skip extension not loaded";
+?>
+--FILE--
+<?php
+
+set_error_handler(function($code, $str) { echo $str . PHP_EOL; });
+
+// fixture came from our signatures.yml
+$sigIn = hex2bin("30440220132382ca59240c2e14ee7ff61d90fc63276325f4cbe8169fc53ade4a407c2fc802204d86fbe3bde6975dd5a91fdc95ad6544dcdf0dab206f02224ce7e2b151bd82ab");
+$msg32 = \pack("H*", "9e5755ec2f328cc8635a55415d0e9a09c2b6f2c9b0343c945fbbfe08247a4cbe");
+$priv = \pack("H*", "cf5f5900d8406e2726dedfc61c7cf43ebad");
+
+$ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+
+$sig = null;
+try {
+    secp256k1_ecdsa_sign($ctx, $sig, $msg32, $priv);
+} catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
+?>
+--EXPECT--
+secp256k1_ecdsa_sign(): Parameter 4 should be 32 bytes
