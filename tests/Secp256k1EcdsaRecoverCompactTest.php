eb6820a42c2e50b2a60cfe8bdefc34a2af09dc46
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-02-17 18:58:29 +0000

    type hints for secp256k1_ecdsa_recoverable_signature_parse_compact

--- tests/Secp256k1EcdsaRecoverCompactTest.php
+++ tests/Secp256k1EcdsaRecoverCompactTest.php
@@ -13,8 +13,7 @@ class Secp256k1EcdsaRecoverCompactTest extends TestCase
 
         $sig = pack("H*", 'fe5fe404f3d8c21e1204a08c38ff3912d43c5a22541d2f1cdc4977cbcad240015a3b6e9040f62cacf016df4fef9412091592e4908e5e3a7bd2a42a4d1be01951');
         /** @var resource $s */
-        $s = '';
-
+        $s = null;
         $this->assertEquals(1, secp256k1_ecdsa_recoverable_signature_parse_compact($context, $s, $sig, $recid));
 
         $privateKey = pack("H*", 'fbb80e8a0f8af4fb52667e51963ac9860c192981f329debcc5d123a492a726af');
