0068de03936ff1d966a9303a1be1f33b67f2e304
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-02-17 17:11:17 +0000

    type hints for secp256k1_ecdsa_signature_serialize_comapct

--- secp256k1/secp256k1.c
+++ secp256k1/secp256k1.c
@@ -100,10 +100,14 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO(arginfo_secp256k1_ecdsa_signature_serialize
     ZEND_ARG_TYPE_INFO(0, ecdsaSignature, IS_RESOURCE, 0)
 ZEND_END_ARG_INFO();
 
-ZEND_BEGIN_ARG_INFO(arginfo_secp256k1_ecdsa_signature_serialize_compact, 0)
+#if (PHP_VERSION_ID >= 70000 && PHP_VERSION_ID <= 70200)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO(arginfo_secp256k1_ecdsa_signature_serialize_compact, IS_LONG, NULL, 0)
+#else
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO(arginfo_secp256k1_ecdsa_signature_serialize_compact, IS_LONG, 0)
+#endif
     ZEND_ARG_TYPE_INFO(0, context, IS_RESOURCE, 0)
-    ZEND_ARG_INFO(1, sig64Out)
-    ZEND_ARG_INFO(0, ecdsaSignature)
+    ZEND_ARG_TYPE_INFO(1, sig64Out, IS_STRING, 1)
+    ZEND_ARG_TYPE_INFO(0, ecdsaSignature, IS_RESOURCE, 0)
 ZEND_END_ARG_INFO();
 
 ZEND_BEGIN_ARG_INFO(arginfo_secp256k1_ecdsa_verify, 0)
@@ -597,18 +601,18 @@ PHP_FUNCTION(secp256k1_ecdsa_signature_serialize_compact)
     secp256k1_context *ctx;
     secp256k1_ecdsa_signature *sig;
     unsigned char *sigOut;
-    int result;
+    int result = 0;
 
     if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rz/r", &zCtx, &zSigOut, &zSig) == FAILURE) {
-        RETURN_FALSE;
+        RETURN_LONG(result);
     }
 
     if ((ctx = php_get_secp256k1_context(zCtx)) == NULL) {
-        RETURN_FALSE;
+        RETURN_LONG(result);
     }
 
     if ((sig = php_get_secp256k1_ecdsa_signature(zSig)) == NULL) {
-        RETURN_FALSE;
+        RETURN_LONG(result);
     }
 
     sigOut = emalloc(COMPACT_SIGNATURE_LENGTH);
