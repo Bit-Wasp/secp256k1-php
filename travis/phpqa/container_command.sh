2441d67e27eb74f83f772ddc9fdcf9f18b0c342a
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-03-12 00:51:31 +0100

    ditch processing lcov output, we submit to codecov now

--- travis/phpqa/container_command.sh
+++ travis/phpqa/container_command.sh
@@ -9,6 +9,4 @@ docker build -t secp256k1build .
 docker run -it \
      -v ${_GCOV_DIR}:/usr/src/php/lcov_html \
      -v ${_EXT_DIR}:/usr/src/php/ext/secp256k1 \
-     -e "COVERAGE_TARGET=${COVERAGE_TARGET}" \
-     -e "COVERAGE_PREFIX_FUNCTIONS=${COVERAGE_PREFIX_FUNCTIONS}" \
      --name s1 secp256k1build ${_CMD}
