206d23692ba45753ad2e49d7772e3317476a6612
Thomas Kerin <me@thomaskerin.io>
2019-07-04 19:44:27 +0100

    leverage build args in Dockerfile for phpqa - different php versions

--- travis/phpqa/check_coverage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./container_command.sh coverage.sh
-docker stop s1; docker rm s1;
--- travis/run_valgrind_test.sh
+++ travis/run_valgrind_test.sh
@@ -5,13 +5,14 @@ if [ "${VALGRIND}" = "true" ]; then
     cd $(git rev-parse --show-toplevel)/secp256k1
     sudo make clean
     cd $(git rev-parse --show-toplevel)/travis/phpqa
+    ./build_container.sh
     ./container_command.sh valgrind.sh
     if [ "$?" != "0" ]; then
         exitHard=true
     fi
     docker stop s1 > /dev/null;
     docker rm s1 > /dev/null;
-    echo "completed coverage command";
+    echo "completed valgrind command";
 fi
 
 if [ "${exitHard}" = "true" ]; then
